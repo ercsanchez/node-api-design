fcbc1568ed47d7cc4893a4b09818962eff065653
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-19 07:20:17 +0800

    3-01 create simple express server

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-  // console.log(req);
-  // console.log(res);
-  if (req.url === "/" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ message: "hello" }));
-    res.end();
-    return;
-    // console.log('hello from server')
-    // res.end()
-  }
-
-  res.writeHead(404, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "nope" }));
-});
+const app = require('./server')
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`server on ${PORT}`);
 });
