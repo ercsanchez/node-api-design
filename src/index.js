1ce6b993115a8b7a6fa566578389049f59ef87a1
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-19 06:29:12 +0800

    2-01a req and res

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+  console.log(req);
+  console.log(res);
   if (req.url === "/" && req.method === "GET") {
     console.log('hello from server')
     res.end()
