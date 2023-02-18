b7ccbe81a357b1bf0029516160b6b1df9d6595f6
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-19 06:58:31 +0800

    2-01 server code using course guide

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  console.log(res);
+  // console.log(req);
+  // console.log(res);
   if (req.url === "/" && req.method === "GET") {
-    console.log('hello from server')
-    res.end()
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ message: "hello" }));
+    res.end();
+    return;
+    // console.log('hello from server')
+    // res.end()
   }
+
+  res.writeHead(404, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "nope" }));
 });
 
 const PORT = process.env.PORT || 5000;
