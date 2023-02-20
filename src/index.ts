04a4093047309d544edc4fa8231a6f5cd549e43e
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-21 06:33:59 +0800

    7-1a create jwt;

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 // const app = require('./server')
 import app from './server'
+import * as dotenv from 'dotenv'
+
+dotenv.config();
+// console.log(process.env) // log all env variables
 
 const PORT = process.env.PORT || 5000;
 
