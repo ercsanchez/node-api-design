025c88c3ce893da55dd3e8f3b1d642b07d41057b
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-20 10:28:38 +0800

    6-1a define REST routes

---
+++ src/router.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+// not a constructor & just a regular function | capitalized coz of bad coding conventions
+const router = Router();
+
+router.get('/product', () => {})
+router.get('/product/:id', () => {})
+router.put('/product/:id', () => {})
+router.post('/product', () => {})
+router.delete('/product/:id', () => {})
+
+router.get('/update', () => {})
+router.get('/update/:id', () => {})
+router.put('/update/:id', () => {})
+router.post('/update', () => {})
+router.delete('/update/:id', () => {})
+
+router.get('/updatepoint', () => {})
+router.get('/updatepoint/:id', () => {})
+router.put('/updatepoint/:id', () => {})
+router.post('/updatepoint', () => {})
+router.delete('/updatepoint/:id', () => {})
+
+export default router;
