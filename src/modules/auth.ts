284236984300a9d6b67f741e8239bec677739f1a
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-21 06:45:17 +0800

    7-1c validate Bearer token;

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -17,4 +17,26 @@ export const protectRoute = (req, res, next) => {
     res.json({message: 'not authorized'})
     return;
   }
+
+  // check if there is a token value after "Bearer" prefix
+  const [, token] = bearer.split(' ');
+
+  if (!token) {
+    res.status(401);
+    res.json({message: 'no token'});
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET); // checks if token is valid based on JWT_SECRET
+
+    // currently doesn't verify user if the same one based on a valid token
+    req.user = user;
+    next();
+    return;
+  } catch (e) {
+    res.status(401);
+    res.json({message: "not valid token"})
+    console.error(e);
+  }
 }
