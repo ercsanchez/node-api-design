68d738ad4da1447f4f28e75664acf3ea6c8347d0
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-22 06:45:53 +0800

    08-1b add validation to route;

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express';
+import { body, validationResult } from "express-validator";
 
 // not a constructor & just a regular function | capitalized coz of bad coding conventions
 const router = Router();
 
 router.get('/product', (req, res) => {res.json({message: 'products'})})
+
 router.get('/product/:id', () => {})
-router.put('/product/:id', () => {})
+router.put(
+  '/product/:id',
+  body('name').isString(),
+  (req, res) => {
+    // console.log("testing validation");
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      res.status(400);
+      res.json({errors: errors.array()})
+    }
+  }) // req.body (a json) should have a field called name
+
 router.post('/product', () => {})
 router.delete('/product/:id', () => {})
 
