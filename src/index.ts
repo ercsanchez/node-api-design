49f22bec0c9dd70db6ea62d1f96806dc9a2eeb7c
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-23 15:44:30 +0800

    10-2 use environment and stage configurations;

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@
 import app from './server'
 import * as dotenv from 'dotenv'
 
+import config from './config';
+
 dotenv.config();
 // console.log(process.env) // log all env variables
 
-const PORT = process.env.PORT || 5000;
+const PORT = config.port
 
 app.listen(PORT, () => {
   console.log(`server on ${PORT}`);
